634379e494f9a31735cacf1cce0a24f1cf1e5b7c
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-03-03 16:30:36 +0200

    Implement all functions as recursive

---
+++ UnitTest_8_1_rec/UnitTest_8_1_rec.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8.1_rec/lab_8.1_rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81rec
+{
+	TEST_CLASS(UnitTest81rec)
+	{
+	public:
+		
+    TEST_METHOD(TestCountIncludes)
+    {
+      char str[] = "BASIC AND Delphi";
+      char letters[] = "BASIC";
+
+      const int count = CountIncludes(str, letters);
+
+      Assert::AreEqual(6, count);
+    };
+
+    TEST_METHOD(TestChange)
+    {
+      char str[] = "BASIC, Python and BASIC";
+      char letters[] = "BASIC";
+
+      char* dest = new char[30];
+      dest = Change(str, "BASIC", "Delphi");
+
+      Assert::AreEqual("Delphi, Python and Delphi", dest);
+    };
+  };
+}
