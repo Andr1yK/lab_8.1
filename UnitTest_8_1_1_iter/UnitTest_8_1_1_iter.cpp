0430a3dcdde382f533951c0322acf165a881da16
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-03-03 13:23:22 +0200

    Добавьте файлы проекта.

---
+++ UnitTest_8_1_1_iter/UnitTest_8_1_1_iter.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8.1.1/lab_8.1.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest811iter
+{
+  TEST_CLASS(UnitTest811iter)
+  {
+  public:
+    
+    TEST_METHOD(TestCountIncludes)
+    {
+      char str[] = "BASIC AND Delphi";
+      char letters[] = "BASIC";
+
+      const int count = CountIncludes(str, letters);
+
+      Assert::AreEqual(6, count);
+    };
+
+    TEST_METHOD(TestChange)
+    {
+      char str[] = "BASIC AND Python";
+      char letters[] = "BASIC";
+
+      char* dest = new char[30];
+      dest = Change(str, "BASIC", "Delphi");
+
+      Assert::AreEqual("Delphi AND Python", dest);
+    };
+  };
+}
